4c5b870672279ee48bbafacd375683f1db426fb3
Mishchenko-Andrey <122mishchenko@mail.ru>
2025-02-13 02:46:56 +0700

    DZ-6 Домашняя работа №6. Циклы

--- src/Main.java
+++ src/Main.java
@@ -1,73 +1,66 @@
 public class Main {
     public static void main(String[] args) {
-        
-//Домашнее задание  № 5. Условный оператор часть 2
-        // Задача №1-N2
 
-        int clientOS = 1;
-        int clientDeviceYear = 2014;
+//Домашнее задание  № 6. Циклы
+        // Задача №1
+        for (int i = 0; i < 11; i++) {
+            System.out.println("Интерация цикла " + i);
+        }
 
-        if (clientOS == 0 && clientDeviceYear < 2015) {
-            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        } else if (clientOS == 0 && clientDeviceYear >= 2015) {
-            System.out.println("Установите версию приложения для iOS по ссылке");
-        } else if (clientOS == 1 && clientDeviceYear < 2015) {
-            System.out.println("Установите облегченную версию приложения для Android по ссылке");
-        }  if (clientOS == 1 && clientDeviceYear >= 2015) {
-            System.out.println("Установите версию приложения для Android по ссылке");
+        //Задача №2
 
+        for (int i = 10; i >= 0; i--) {
+            System.out.println("Обратный цикл " + i);
         }
 
         //Задача №3
-
-        int year = 2025;
-        if (year <= 1584) {
-            System.out.println(year + " год не является високосным, так как високосные года введены с 1584 года.");
-        } else if ((year % 400 == 0) || (year % 100 != 0 && year % 4 == 0)) {
-            System.out.println(year + " год является високосным");
-        } else {
-            System.out.println(year + " год не является високосным");
+        for (int i = 0; i < 17; i = i + 2) {
+            System.out.println("Чётные числа " + i);
         }
 
         //Задача №4
 
-        int deliveryDistance = 95;
-        if (deliveryDistance < 20) {
-            System.out.println("Доставка занимается сутки");
-        } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
-            System.out.println("Потрубется 2 дня");
-        } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
-            System.out.println("Потребуется 3 дня");
-        } else {
-            System.out.println("Доставки нет");
+        for (int i = 10; i >= -10; i--) {
+            System.out.println("От большего к меньшему" + i);
         }
-
         //Задача №5
 
-        int monthNumber = 12;
-        switch (monthNumber) {
-            case 12:
-            case 1:
-            case 2:
-                System.out.println("Зима");
-                break;
-            case 3:
-            case 4:
-            case 5:
-                System.out.println("Весна");
-                break;
-            case 6:
-            case 7:
-            case 8:
-                System.out.println("Лето");
-                break;
-            case 9:
-            case 10:
-            case 11:
-                System.out.println("Осень");
-                break;
-            default:
-                System.out.println("Некорректный номер месяца");
+        for (int i = 1904; i <= 2096; i = i + 4) {
+            System.out.println("Високосный год " + i);
+        }
+
+        //Задача №6
+        for (int i = 7; i <= 98; i = i + 7) {
+            System.out.println("Последовательность чисел " + i);
+        }
+
+        //Задача №7
+
+        for (int i = 1; i <= 512; i = i * 2) {
+            System.out.println("Последовательность чисел " + i);
+        }
+
+        //Задача №8
+        int savings = 29000;
+        int total = 0;
+        for (int i = 1; i < 13; i++) {
+            total = total + savings;
+            System.out.println("Месяц " + i  + " Итого " +   total);}
+        //Задача №9
+
+        int savings2 = 29000;
+        int total2 = 0;
+        for (int i = 1; i < 13; i++) {
+            total2 = total2 + total/12;
+            total2 = total2 + savings2;
+            System.out.println("Месяц " + i + " Итого " + total2);}
+
+        //Задача №10
+
+        int number = 2;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number + " * " + i +  " = " + (number * i));
+
         }
     }
 }
