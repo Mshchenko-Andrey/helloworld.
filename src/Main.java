143f27f461c0c9c56af57dc788aaf2622ec240b2
Mishchenko-Andrey <122mishchenko@mail.ru>
2025-03-17 23:18:27 +0700

    Домашнее задание  № 13. Методы объектов

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,16 @@
-import java.time.LocalDate;
-import java.util.Arrays;
-
 public class Main {
 
-    //Домашнее Задание №12. Объекты и классы
+    //Домашнее Задание №13. Методы объектов
     public static void main(String[] args) {
-
+        // Создаем авторов
         Author author1 = new Author("Джоан", "Роулинг");
         Author author2 = new Author("Джон", "Толкин");
 
-
+        // Создаем книги
         Book book1 = new Book("Гарри Поттер", author1, 1996);
         Book book2 = new Book("Хоббит", author2, 1937);
 
-
+        // Выводим информацию о книгах
         System.out.println("Книга 1: " + book1.getTitle() + ", Автор: " +
                 book1.getAuthor().getFirstName() + " " +
                 book1.getAuthor().getLastName() + ", Год: " +
@@ -24,9 +21,19 @@ public class Main {
                 book2.getAuthor().getLastName() + ", Год: " +
                 book2.getYear());
 
-
-        book1.setYear(1997);
+        // Изменяем год публикации первой книги
+        book1.setYear(1873);
         System.out.println("Обновленный год публикации книги 1: " + book1.getYear());
+
+        System.out.println(book1.toString());
+        System.out.println(book2.toString());
+
+        System.out.println("book1 equals book2: " + book1.equals(book2));
+
+        System.out.println("HashCode book1: " + book1.hashCode());
+        System.out.println("HashCode book2: " + book2.hashCode());
+
+
     }
 }
 
